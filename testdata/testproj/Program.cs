899f071d8e8dbb21d80ae6c7e79a5812c0cf9839
Chris Helmich <kagekirin@gmail.com>
2022-04-18 13:22:22 +0900

    file: add testdata

---
+++ testdata/testproj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
